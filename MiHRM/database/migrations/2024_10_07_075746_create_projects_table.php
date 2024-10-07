51e3fd87fa65450ae46adf91c680a44013a78059
Talha <talhayousafsaeed0@gmail.com>
2024-10-07 13:05:15 +0500

    New migrations Salaries,Projects

---
+++ MiHRM/database/migrations/2024_10_07_075746_create_projects_table.php
@@ -0,0 +1,36 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('projects', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('employee_id')->constrained('employees')->onDelete('cascade');
+            $table->string('title');
+            $table->string('description');
+            $table->enum('status', ['in_progress', 'pending', 'completed']);
+            $table->date('deadline');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('projects');
+    }
+};
