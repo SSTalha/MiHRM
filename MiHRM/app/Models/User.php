9cb1155bc2881df82ab3a55561bec209513ea5a8
Talha <talhayousafsaeed0@gmail.com>
2024-10-07 11:07:24 +0500

    Made some changes to config/auth and User Model

--- MiHRM/app/Models/User.php
+++ MiHRM/app/Models/User.php
@@ -7,10 +7,13 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 use Laravel\Sanctum\HasApiTokens;
+use Spatie\Permission\Traits\HasRoles;
+use Tymon\JWTAuth\Contracts\JWTSubject;
+use Illuminate\Auth\Passwords\CanResetPassword;
 
-class User extends Authenticatable
+class User extends Authenticatable implements JWTSubject
 {
-    use HasApiTokens, HasFactory, Notifiable;
+    use HasApiTokens, HasFactory, Notifiable, HasRoles,CanResetPassword;
 
     /**
      * The attributes that are mass assignable.
@@ -41,4 +44,18 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+            /**
+     *  JWT abstract Methods
+     */
+
+     public function getJWTIdentifier()
+     {
+         return $this->getKey();
+     }
+ 
+     public function getJWTCustomClaims()
+     {
+         return [];
+     }
 }
